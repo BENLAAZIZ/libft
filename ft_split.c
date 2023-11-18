b5234f031b18dde394305786dbe1ee56daf8b053
Hamza Ben laziz <hben-laz@e2r4p12.1337.ma>
2023-11-18 19:02:33 +0100

    update

--- ft_split.c
+++ ft_split.c
@@ -6,96 +6,111 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 22:21:22 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/11/16 19:41:44 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/11/18 18:59:59 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int count_words(char const *s, char c)
+static int	count_word(const char *s, char c)
 {
-    int len = 0;
-    int i =0;
-    if(s[0] == c)
-      len  = 0;
-    if(s[0] == '\0')
-      return (0);
-    if(s[0] != c)
-        len++;
-    i = 1;
-    while(s[i])
-    {
-        if(s[i - 1] == c && s[i] != c)
-          len++;
-        i++;  
-    }
-    return (len);
-}
-static char *ft_charge(char const *s,int start, int size)
-{
-    char *tab = malloc(size + 1);
-    if(!tab)
-     return (NULL);
-    int i =0;
-    while(i < size)
-    {
-        tab[i] = s[start + i];
-        i++;
-    }
-    tab[i] = '\0';
-    return  (tab);
+	int	len;
+	int	i;
+
+	len = 0;
+	i = 0;
+	if (s[0] == c)
+		i++;
+	else
+		len++;
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] == c && s[i] != c)
+			len++;
+		i++;
+	}
+	return (len);
 }
 
-char **ft_split(char const *s, char c)
+static char	*charge_word(const char *s, int start, int end)
 {
-    char **array;
-    int size = count_words(s,c);
-    array = (char **)malloc(sizeof(char *) * (size + 1));
-    if(!array)
-       return (NULL);
+	int		i;
+	int		size;
+	char	*ptr;
 
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while(i < size)
-    {
-        while(s[j] && s[j] == c)
-            j++;
-        k = j;    
-        while(s[k] != c && s[k])
-           k++; 
-        k--;   
-        array[i] = ft_charge(s, j, k - j + 1);
-        if(!array[i])
-        {
-            while(i)
-            {
-                i--;
-                free(array[i]);
-            }
-            free(array);
-            return (0);
-        }
-        j = k + 1;
-        i++;
-    }
-    array[i] = 0;
-    return (array);
+	i = 0;
+	size = end - start;
+	ptr = malloc(size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		ptr[i] = s[start];
+		i++;
+		start++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+static void	free_function(char **array, int i)
+{
+	i--;
+	while (i >= 0)
+	{
+		free(array[i]);
+		i--;
+	}
+	free(array);
+}
+
+char	**my_array(char **array, char const *s, char c, int nbr_word)
+{
+	int		i;
+	int		j;
+	int		end;
+
+	i = -1;
+	j = 0;
+	while (++i < nbr_word)
+	{
+		while (s[j] == c && s[j] != '\0')
+			j++;
+		end = j;
+		while (s[end] != c && s[end] != '\0')
+			end++;
+		array[i] = charge_word(s, j, end);
+		if (array[i] == NULL)
+		{
+			free_function(&array[i], i);
+			return (0);
+		}
+		j = end;
+	}
+	array[i] = NULL;
+	return (array);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+	int		nbr_wrd;
+
+	nbr_wrd = count_word(s, c);
+	array = (char **)malloc(sizeof(char *) * (nbr_wrd + 1));
+	if (array == NULL)
+		return (NULL);
+	return (my_array(array, s, c, nbr_wrd));
 }
 // int main()
 // {
-     
-//     const char *s = "---ayoub----ben----hamza--hhhh----hdhdh-sdhdshhds-sdjdjsf";
-//     char **array = ft_split(s,'-');
-
+//     int n;
+//     const char *s = "----h--amza--nkin--ben--laazziz";
+//     char **array= ft_split(s,'-');
 //     while(*array)
 //     {
-//         printf("%s\n",*array);
-//         array++;
+//     printf("%s\n", *array);
+//     array++;
+
 //     }
-//     return (0);
 // }
-
-
-// *s = "---ayoub----ben----hamza--"
-//       0123456789
