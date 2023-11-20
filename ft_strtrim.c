d9f63d85521fd709f5a25b83ac8b344b58a00ae6
Hamza Ben laziz <hben-laz@e1r3p8.1337.ma>
2023-11-20 20:39:47 +0100

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:00:23 by hben-laz          #+#    #+#             */
-/*   Updated: 2023/11/19 19:23:37 by hben-laz         ###   ########.fr       */
+/*   Updated: 2023/11/20 20:37:53 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,27 +31,31 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	i;
 	size_t	len_s1;
 	size_t	j;
-	char	*tab;
+	// char	*tab;
 
 	if (s1 == NULL) 
 		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
 	len_s1 = ft_strlen(s1);
-	while (len_s1 > 0 && chek_set(set, s1[len_s1 - 1]))
-		len_s1--;
+	
 	i = 0;
 	while (s1[i] != '\0' && chek_set(set, s1[i]))
 		i++;
-	j = len_s1 - i + 1;
-	tab = (char *)malloc(j + 1);
-	if (tab == NULL)
-		return (NULL);
-	j = 0;
-	while (i < len_s1)
-		tab[j++] = s1[i++];
-	tab[j] = '\0';
-	return (tab);
+	if (i == len_s1)
+        return ft_strdup("");
+	while (len_s1 > 0 && chek_set(set, s1[len_s1 - 1]))
+		len_s1--;
+	j = len_s1 - i;
+	return (ft_substr(s1, i, j));
+	// tab = (char *)malloc(j + 1);
+	// if (tab == NULL)
+	// 	return (NULL);
+	// j = 0;
+	// while (i < len_s1)
+	// 	tab[j++] = s1[i++];
+	// tab[j] = '\0';
+	// return (ft_substr(i, ));
 }
 // #include <stdio.h>
 // int main()
